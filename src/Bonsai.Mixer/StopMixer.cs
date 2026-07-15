533771ed1d569b33c297caf8551637eeba8fe2d1
glopesdev <g.lopes@neurogears.org>
2026-07-15 11:19:52 +0100

    Rename MixerStreamContext to MixerContext

--- src/Bonsai.Mixer/StopMixer.cs
+++ src/Bonsai.Mixer/StopMixer.cs
@@ -5,24 +5,24 @@ using System.Reactive.Linq;
 namespace Bonsai.Mixer
 {
     /// <summary>
-    /// Represents an operator that stops audio processing on every mixer stream
-    /// context in the sequence.
+    /// Represents an operator that stops audio processing on every mixer context
+    /// in the sequence.
     /// </summary>
-    [Description("Stops audio processing on every mixer stream context in the sequence.")]
-    public class StopMixer : Sink<MixerStreamContext>
+    [Description("Stops audio processing on every mixer context in the sequence.")]
+    public class StopMixer : Sink<MixerContext>
     {
         /// <summary>
-        /// Stops audio processing on every mixer stream context in an observable sequence.
+        /// Stops audio processing on every mixer context in an observable sequence.
         /// </summary>
         /// <param name="source">
-        /// A sequence of <see cref="MixerStreamContext"/> objects.
+        /// A sequence of <see cref="MixerContext"/> objects.
         /// </param>
         /// <returns>
         /// An observable sequence that is identical to the <paramref name="source"/>
         /// sequence but where there is an additional side effect of stopping
-        /// audio processing on every mixer stream context in the sequence.
+        /// audio processing on every mixer context in the sequence.
         /// </returns>
-        public override IObservable<MixerStreamContext> Process(IObservable<MixerStreamContext> source)
+        public override IObservable<MixerContext> Process(IObservable<MixerContext> source)
         {
             return source.Do(mixer => mixer.Stop());
         }
